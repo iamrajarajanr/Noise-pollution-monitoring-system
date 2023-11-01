e0d94643561756ae72fdb39416d098c098bb0699
RAJARAJAN R <141346963+iamrajarajanr@users.noreply.github.com>
2023-11-01 10:56:22 +0530

    Update Flask web framework.py

--- Phase5/Flask web framework.py
+++ Phase5/Flask web framework.py
@@ -1,1 +1,22 @@
+# Import necessary libraries
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name)
+
+# Simulated data from IoT sensors
+noise_data = {"sensor1": 70, "sensor2": 60, "sensor3": 75}
+
+@app.route('/')
+def index():
+    return render_template('index.html', data=noise_data)
+
+@app.route('/update', methods=['POST'])
+def update_data():
+    sensor_id = request.form['sensor_id']
+    noise_level = request.form['noise_level']
+    noise_data[sensor_id] = float(noise_level)
+    return jsonify({"message": "Data updated successfully"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
 
